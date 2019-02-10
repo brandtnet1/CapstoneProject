8b08fbf70d2d6d3a4b56e6f69697673bb3fee891
Kenny Nguyen <knguyen0310@gmail.com>
2019-02-10 15:44:09 -0500

    added another picker!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Select, TimePicker } from 'antd';
+import { Table, Select, Row, Col, Slider, TimePicker, InputNumber } from 'antd';
 
 import "antd/dist/antd.css";
 //import "./index.css";
@@ -43,6 +43,7 @@ class App extends Component {
   
   render() {
     const { Option, OptGroup } = Select;
+    const { inputValue } = this.state;
     //dataSource for table - currently only has dummy data
     //when the server is finished, delete the dummy data here and the line below should be all that is needed
     //const { data } = this.state;
@@ -142,110 +143,124 @@ class App extends Component {
     }];
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Rollins Course Schedule</h1>
-          <div>
-          <Select
-            mode="multiple"
-            style={{ width: '15%' }}
-            placeholder="Select day of the week..."
-            onSelectDay={this.handleSelectDay}
-          >
-            <Option value="monday">Monday</Option>
-            <Option value="tuesday">Tuesday</Option>
-            <Option value="wednesday">Wednesday</Option>
-            <Option value="thursday">Thursday</Option>
-            <Option value="friday">Friday</Option>
-            <Option value="saturday">Saturday</Option>
-          </Select>
-          <TimePicker
-            style={{ width: '15%' }}
-            placeholder="Select time of the day..."
-            minuteStep={5}
-            format = 'hh:mm a'
-            use12Hours
-            onSelectDay={this.handleSelectDay}
-          >
-          </TimePicker >
-          <Select
-            mode="multiple"
-            style={{ width: '20%' }}
-            placeholder="Select department/major (try search!)..."
-            onSelectDeparment={this.handleSelectDeparment}
-          >
-            <Option value="american-studes">American Studies</Option>
-            <Option value="anthropology">Anthropology</Option>
-            <Option value="art-histoy">Art & Art History</Option>
-            <Option value="biology">Biology</Option>
-            <Option value="business">Business</Option>
-            <Option value="career">Career And Life Planning</Option>
-            <Option value="biochemistry">Biochemistry</Option>
-            <Option value="chemistry">Chemistry</Option>
-            <Option value="classical-studies">Classical Studies</Option>
-            <Option value="communication">Communication</Option>
-            <Option value="computer-science">Computer Science</Option>
-            <Option value="cricital-media-cultural">Critical Media & Cultural Studies</Option>
-            <Option value="economics">Economics</Option>
-            <Option value="educatiom">Education</Option>
-            <Option value="english">English</Option>
-            <Option value="environmental">Environmental Studies</Option>
-            <Option value="film">Film Studies</Option>
-            <Option value="globa-health">Global Health</Option>
-            <Option value="health-professions">Health Professions Advising</Option>
-            <Option value="health-phyiscal">Health and Physical Education </Option>
-            <Option value="history">History</Option>
-            <Option value="honors">Honors Degree Program</Option>
-            <Option value="it">Information Technology</Option>
-            <Option value="interdisciplinary">Interdisciplinary</Option>
-            <Option value="latin-amerian-caribbean">Latin American & Caribbean</Option>
-            <Option value="mathematics">Mathematics</Option>
-            <Option value="middle-eastern-north-african">Middle Eastern & North African Studies</Option>
-            <OptGroup label="Modern Languages & Literatures">
-              <Option value="arabic">Arabic</Option>
-              <Option value="chinese">Chinese</Option>
-              <Option value="french">French</Option>
-              <Option value="german">German</Option>
-              <Option value="japanese">Japnese</Option>
-              <Option value="spanish">Spanish</Option>
-            </OptGroup>
-            <Option value="music">Music</Option>
-            <Option value="neuroscience">Neuroscience</Option>
-            <Option value="off-campus">Off-Campus Program Courses</Option>
-            <Option value="philosophy">Philosophy</Option>
-            <Option value="religion">Religion</Option>
-            <Option value="physics">Physics</Option>
-            <Option value="political-science">Political Science</Option>
-            <Option value="psychology">Psychology</Option>
-            <Option value="rcc">Rollins Conference Courses</Option>
-            <Option value="swag">Sexuality Women's & Gender Studies</Option>
-            <Option value="social-entrepreneurship">Social Entrepreneurship</Option>
-            <Option value="social-innovation">Social Innovation</Option>
-            <Option value="sociology">Sociology</Option>
-            <Option value="resource">Student Resource Center</Option>
-            <Option value="theatre">Theatre Arts</Option>
-            <Option value="dance">Dance</Option>
-            <OptGroup label="rFLA">
-              <Option value="rfsla">rFLA</Option>
-              <Option value="ice">ICE</Option>
-              <Option value="imw">IMW</Option>
-              <Option value="mm">MM</Option>
-              <Option value="wcc">WCC</Option>
-            </OptGroup>
-          </Select>
-          <Select
-            mode="multiple"
-            style={{ width: '15%' }}
-            placeholder="Select course level (100, 200, etc.)..."
-            onSelectLevel={this.handleSelectLevel}
-          >
-            <Option value="100">100+</Option>
-            <Option value="200">200+</Option>
-            <Option value="300">300+</Option>
-            <Option value="400">400+</Option>
-          </Select>
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">Rollins Course Schedule</h1>
+          <div className = "ui-selectors">
+            <Select
+              mode="multiple"
+              style={{ width: '15%' }}
+              placeholder="Select day of the week..."
+              onChange={this.handleSelectDay}
+            >
+              <Option value="monday">Monday</Option>
+              <Option value="tuesday">Tuesday</Option>
+              <Option value="wednesday">Wednesday</Option>
+              <Option value="thursday">Thursday</Option>
+              <Option value="friday">Friday</Option>
+              <Option value="saturday">Saturday</Option>
+            </Select>
+
+            <TimePicker
+              style={{ width: '15%' }}
+              placeholder="Select start time..."
+              minuteStep={5}
+              format = 'hh:mm a'
+              use12Hours
+              onChange={this.handleSelectDay}
+            >
+            </TimePicker >
+            <TimePicker
+              style={{ width: '15%' }}
+              placeholder="Select end time..."
+              minuteStep={5}
+              format = 'hh:mm a'
+              use12Hours
+              onChange={this.handleSelectDay}
+            >
+            </TimePicker >
+
+            <Select
+              mode="multiple"
+              style={{ width: '20%' }}
+              placeholder="Select department/major (try search!)..."
+              onChange={this.handleSelectDeparment}
+            >
+              <Option value="american-studes">American Studies</Option>
+              <Option value="anthropology">Anthropology</Option>
+              <Option value="art-histoy">Art & Art History</Option>
+              <Option value="biology">Biology</Option>
+              <Option value="business">Business</Option>
+              <Option value="career">Career And Life Planning</Option>
+              <Option value="biochemistry">Biochemistry</Option>
+              <Option value="chemistry">Chemistry</Option>
+              <Option value="classical-studies">Classical Studies</Option>
+              <Option value="communication">Communication</Option>
+              <Option value="computer-science">Computer Science</Option>
+              <Option value="cricital-media-cultural">Critical Media & Cultural Studies</Option>
+              <Option value="economics">Economics</Option>
+              <Option value="educatiom">Education</Option>
+              <Option value="english">English</Option>
+              <Option value="environmental">Environmental Studies</Option>
+              <Option value="film">Film Studies</Option>
+              <Option value="globa-health">Global Health</Option>
+              <Option value="health-professions">Health Professions Advising</Option>
+              <Option value="health-phyiscal">Health and Physical Education </Option>
+              <Option value="history">History</Option>
+              <Option value="honors">Honors Degree Program</Option>
+              <Option value="it">Information Technology</Option>
+              <Option value="interdisciplinary">Interdisciplinary</Option>
+              <Option value="latin-amerian-caribbean">Latin American & Caribbean</Option>
+              <Option value="mathematics">Mathematics</Option>
+              <Option value="middle-eastern-north-african">Middle Eastern & North African Studies</Option>
+              <OptGroup label="Modern Languages & Literatures">
+                <Option value="arabic">Arabic</Option>
+                <Option value="chinese">Chinese</Option>
+                <Option value="french">French</Option>
+                <Option value="german">German</Option>
+                <Option value="japanese">Japnese</Option>
+                <Option value="spanish">Spanish</Option>
+              </OptGroup>
+              <Option value="music">Music</Option>
+              <Option value="neuroscience">Neuroscience</Option>
+              <Option value="off-campus">Off-Campus Program Courses</Option>
+              <Option value="philosophy">Philosophy</Option>
+              <Option value="religion">Religion</Option>
+              <Option value="physics">Physics</Option>
+              <Option value="political-science">Political Science</Option>
+              <Option value="psychology">Psychology</Option>
+              <Option value="rcc">Rollins Conference Courses</Option>
+              <Option value="swag">Sexuality Women's & Gender Studies</Option>
+              <Option value="social-entrepreneurship">Social Entrepreneurship</Option>
+              <Option value="social-innovation">Social Innovation</Option>
+              <Option value="sociology">Sociology</Option>
+              <Option value="resource">Student Resource Center</Option>
+              <Option value="theatre">Theatre Arts</Option>
+              <Option value="dance">Dance</Option>
+              <OptGroup label="rFLA">
+                <Option value="rfsla">rFLA</Option>
+                <Option value="ice">ICE</Option>
+                <Option value="imw">IMW</Option>
+                <Option value="mm">MM</Option>
+                <Option value="wcc">WCC</Option>
+              </OptGroup>
+            </Select>
+            
+            <Select
+              mode="multiple"
+              style={{ width: '15%' }}
+              placeholder="Select course level (100, 200, etc.)..."
+              onChange={this.handleSelectLevel}
+            >
+              <Option value="100">100+</Option>
+              <Option value="200">200+</Option>
+              <Option value="300">300+</Option>
+              <Option value="400">400+</Option>
+            </Select>
           </div>
-          <Table dataSource={data} columns={columns} />
+          <div className = 'table'>
+            <Table dataSource={data} columns={columns} />
+          </div>
         </header>
         <p className="App-intro">{this.state.data}</p>
       </div>
