b7a3786f96479c6457c83821846e8a10328712b5
Kenny Nguyen <knguyen0310@gmail.com>
2019-02-14 16:27:30 -0500

    added layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Select, TimePicker, BackTop } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon, Table, Select, TimePicker, BackTop } from 'antd';
 
 import "antd/dist/antd.css";
 //import "./index.css";
@@ -48,6 +48,8 @@ class App extends Component {
   
   render() {
     const { Option, OptGroup } = Select;
+    const { SubMenu } = Menu;
+    const { Header, Content, Sider } = Layout;
     //dataSource for table - currently only has dummy data
     //when the server is finished, delete the dummy data here and the line below should be all that is needed
     //const { data } = this.state;
@@ -148,9 +150,49 @@ class App extends Component {
 
     return (
       <div className="app">
-        <header className="app-header">
-          <h1 className="app-title">Rollins Course Schedule</h1>
-          <div className="ui-selectors">
+        <Layout>
+            <Header className="header">
+              <div className="logo" />
+              <h1 className="app-title">Rollins Course Schedule</h1>
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}
+                style={{ lineHeight: '64px' }}
+              >
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+              </Menu>
+            </Header>
+        <Layout>
+      <Sider width={200} style={{ background: '#fff' }}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+        >
+          <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+            <Menu.Item key="9">option9</Menu.Item>
+            <Menu.Item key="10">option10</Menu.Item>
+            <Menu.Item key="11">option11</Menu.Item>
+            <Menu.Item key="12">option12</Menu.Item>
+          </SubMenu>
+        </Menu>
+        <div className="ui-selectors">
 
           <Select
               mode="multiple"
@@ -273,13 +315,28 @@ class App extends Component {
               <Option value="300">300s</Option>
               <Option value="400">400s</Option>
             </Select>
-            <BackTop />
+            
           </div>
+      </Sider>
+      <Layout style={{ padding: '0 24px 24px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content style={{
+          background: '#fff', padding: 24, margin: 0, minHeight: 280,
+        }}
+        >
           <div className='table'>
             <Table dataSource={data} columns={columns} />
           </div>
-        </header>
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
         <p className="App-intro">{this.state.data}</p>
+        <BackTop />
       </div>
     );
   }
