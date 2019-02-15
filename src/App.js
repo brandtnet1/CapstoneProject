173a8278245fb166087d137501b90083615e6d6c
Kenny Nguyen <knguyen0310@gmail.com>
2019-02-14 23:35:16 -0500

    added sidebar, header, and some more dummy data to test scrolling behaviors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Breadcrumb, Icon, Table, Select, TimePicker, BackTop } from 'antd';
+import { Input, Icon, Layout, Table, Select, TimePicker, BackTop } from 'antd';
 
 import "antd/dist/antd.css";
 //import "./index.css";
 
 class App extends Component {
   state = {
-    data: null
+    data: null,
+    collapsed: false
   };
 
   componentDidMount() {
@@ -25,6 +26,16 @@ class App extends Component {
     }
     return body;
   };
+
+  toggleSider = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
+  handleSearch(value) {
+    console.log(`searched ${value}`);
+  }
 
   handleSelectStatus(value) {
     console.log(`selected ${value}`);
@@ -48,8 +59,8 @@ class App extends Component {
   
   render() {
     const { Option, OptGroup } = Select;
-    const { SubMenu } = Menu;
     const { Header, Content, Sider } = Layout;
+    const Search = Input.Search;
     //dataSource for table - currently only has dummy data
     //when the server is finished, delete the dummy data here and the line below should be all that is needed
     //const { data } = this.state;
@@ -81,6 +92,84 @@ class App extends Component {
       comments:'Co-requisite: CMS 167'
     }, {
       key: '3',
+      status: 'Open',
+      seats: 4,
+      course: '10104 CMS 170 1',
+      title: 'Problem Solving II: Java + Lab',
+      credits:'5',
+      time:'1 :00-1 :50P\n2 :30-3 :45P',
+      days:'F\nMW',
+      location:'BUSH 301\nBUSH201',
+      instructor:'D Myers',
+      gened:'',
+      comments:'CMS 167 & 167L'
+    }, {
+      key: '4',
+      status: 'Open',
+      seats: 9,
+      course: '10106 CMS 270 1',
+      title: 'Object-Oriented Design & Devel',
+      credits:'4',
+      time:' 2 :00-3 :15P',
+      days:'TR',
+      location:'BUSH 302',
+      instructor:'R Elva',
+      gened:'',
+      comments:'CMS 170'
+    }, {
+      key: '5',
+      status: 'Open',
+      seats: 3,
+      course: '10107 CMS 330 1X',
+      title: 'System Software Principles',
+      credits:'4',
+      time:' 2 :30-3 :45P',
+      days:'MW',
+      location:'BUSH 202',
+      instructor:'V Summet',
+      gened:'',
+      comments:'CMS 230'
+    }, {
+      key: '6',
+      status: 'Open',
+      seats: 7,
+      course: '10108 CMS 330 2X',
+      title: 'System Software Principles',
+      credits:'4',
+      time:'6 :45-8 :00P',
+      days:'MW',
+      location:'BUSH 202',
+      instructor:'V Summet',
+      gened:'',
+      comments:'CMS 230'
+    }, {
+      key: '7',
+      status: 'Open',
+      seats: 10,
+      course: '10109 CMS 375 1X',
+      title: 'Database Design & Dev',
+      credits:'4',
+      time:'5 :20-6 :35P',
+      days:'TR',
+      location:'BUSH 302',
+      instructor:'R Elva',
+      gened:'',
+      comments:'CMS 270'
+    }, {
+      key: '8',
+      status: 'Open',
+      seats: 8,
+      course: '10110 CMS 460 1X',
+      title: 'Algorithm Analysis',
+      credits:'4',
+      time:'4 :00-5 :15P',
+      days:'MW',
+      location:'BUSH 228',
+      instructor:'J Carrington',
+      gened:'',
+      comments:'CMS 270 and MAT 140'
+    }, {
+      key: '9',
       status: 'Filled',
       seats: 0,
       course: '10095 CMC 110 1',
@@ -151,192 +240,167 @@ class App extends Component {
     return (
       <div className="app">
         <Layout>
-            <Header className="header">
+          <Header className="header">
+            <div className="logo" />
+            <Search
+            placeholder="input search text"
+            onSearch={this.handleSearch}
+            style={{ width: 200 }}
+            />
+          </Header>
+          <Layout>
+            <Sider 
+            width={200} 
+            style={{ background: '#fff' }} 
+            position='fixed' 
+            overflow='auto'
+            trigger={null}
+            collapsible
+            collapsed={this.state.collapsed}
+            >
               <div className="logo" />
-              <h1 className="app-title">Rollins Course Schedule</h1>
-              <Menu
-                theme="dark"
-                mode="horizontal"
-                defaultSelectedKeys={['2']}
-                style={{ lineHeight: '64px' }}
-              >
-                <Menu.Item key="1">nav 1</Menu.Item>
-                <Menu.Item key="2">nav 2</Menu.Item>
-                <Menu.Item key="3">nav 3</Menu.Item>
-              </Menu>
-            </Header>
-        <Layout>
-      <Sider width={200} style={{ background: '#fff' }}>
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
-        >
-          <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
-          </SubMenu>
-        </Menu>
-        <div className="ui-selectors">
-          <Select
-              mode="multiple"
-              style={{ width: '100%' }}
-              placeholder="Select status..."
-              onChange={this.handleSelectStatus}
+              <div className="ui-selectors">
+                <Select
+                    mode="multiple"
+                    style={{ width: '100%' }}
+                    placeholder="Select status..."
+                    onChange={this.handleSelectStatus}
+                  >
+                    <Option value="open">Open</Option>
+                    <Option value="filled">Filled</Option>
+                    <Option value="cancelled">Cancelled</Option>
+                  </Select>
+                  <Select
+                    mode="multiple"
+                    style={{ width: '100%' }}
+                    placeholder="Select day of the week..."
+                    onChange={this.handleSelectDay}
+                  >
+                    <Option value="monday">Monday</Option>
+                    <Option value="tuesday">Tuesday</Option>
+                    <Option value="wednesday">Wednesday</Option>
+                    <Option value="thursday">Thursday</Option>
+                    <Option value="friday">Friday</Option>
+                    <Option value="saturday">Saturday</Option>
+                  </Select>
+                  <TimePicker
+                    style={{ width: '100%' }}
+                    placeholder="Select start time..."
+                    minuteStep={5}
+                    format = 'hh:mm a'
+                    use12Hours
+                    onChange={this.handleSelectDay}
+                  >
+                  </TimePicker >
+                  <TimePicker
+                    style={{ width: '100%' }}
+                    placeholder="Select end time..."  
+                    minuteStep={5}
+                    format = 'hh:mm a'
+                    use12Hours
+                    onChange={this.handleSelectDay}
+                  >
+                  </TimePicker >
+                  <Select
+                    mode="multiple"
+                    style={{ width: '100%' }}
+                    placeholder="Select department/major (you can also search here!)..."
+                    onChange={this.handleSelectDeparment}
+                  >
+                    <Option value="american">American Studies</Option>
+                    <Option value="anthropology">Anthropology</Option>
+                    <Option value="art-histoy">Art & Art History</Option>
+                    <Option value="biology">Biology</Option>
+                    <Option value="business">Business</Option>
+                    <Option value="career">Career And Life Planning</Option>
+                    <Option value="biochemistry">Biochemistry</Option>
+                    <Option value="chemistry">Chemistry</Option>
+                    <Option value="classical-studies">Classical Studies</Option>
+                    <Option value="communication">Communication</Option>
+                    <Option value="computer-science">Computer Science</Option>
+                    <Option value="cricital-media-cultural">Critical Media & Cultural Studies</Option>
+                    <Option value="economics">Economics</Option>
+                    <Option value="education">Education</Option>
+                    <Option value="english">English</Option>
+                    <Option value="environmental">Environmental Studies</Option>
+                    <Option value="film">Film Studies</Option>
+                    <Option value="global-health">Global Health</Option>
+                    <Option value="health-professions">Health Professions Advising</Option>
+                    <Option value="health-phyiscal">Health and Physical Education </Option>
+                    <Option value="history">History</Option>
+                    <Option value="honors">Honors Degree Program</Option>
+                    <Option value="it">Information Technology</Option>
+                    <Option value="interdisciplinary">Interdisciplinary</Option>
+                    <Option value="latin-amerian-caribbean">Latin American & Caribbean</Option>
+                    <Option value="mathematics">Mathematics</Option>
+                    <Option value="middle-eastern-north-african">Middle Eastern & North African Studies</Option>
+                    <OptGroup label="Modern Languages & Literatures">
+                      <Option value="arabic">Arabic</Option>
+                      <Option value="chinese">Chinese</Option>
+                      <Option value="french">French</Option>
+                      <Option value="german">German</Option>
+                      <Option value="japanese">Japnese</Option>
+                      <Option value="spanish">Spanish</Option>
+                    </OptGroup>
+                    <Option value="music">Music</Option>
+                    <Option value="neuroscience">Neuroscience</Option>
+                    <Option value="off-campus">Off-Campus Program Courses</Option>
+                    <Option value="philosophy">Philosophy</Option>
+                    <Option value="religion">Religion</Option>
+                    <Option value="physics">Physics</Option>
+                    <Option value="political-science">Political Science</Option>
+                    <Option value="psychology">Psychology</Option>
+                    <Option value="rcc">Rollins Conference Courses</Option>
+                    <Option value="swag">Sexuality Women's & Gender Studies</Option>
+                    <Option value="social-entrepreneurship">Social Entrepreneurship</Option>
+                    <Option value="social-innovation">Social Innovation</Option>
+                    <Option value="sociology">Sociology</Option>
+                    <Option value="resource">Student Resource Center</Option>
+                    <Option value="theatre">Theatre Arts</Option>
+                    <Option value="dance">Dance</Option>
+                    <OptGroup label="rFLA">
+                      <Option value="rfla">rFLA</Option>
+                      <Option value="ice">ICE</Option>
+                      <Option value="imw">IMW</Option>
+                      <Option value="mm">MM</Option>
+                      <Option value="wcc">WCC</Option>
+                    </OptGroup>
+                  </Select>
+                  <Select
+                    mode="multiple"
+                    style={{ width: '100%' }}
+                    placeholder="Select course level (100, 200, etc.)..."
+                    onChange={this.handleSelectLevel}
+                  >
+                    <Option value="100">100s</Option>
+                    <Option value="200">200s</Option>
+                    <Option value="300">300s</Option>
+                    <Option value="400">400s</Option>
+                  </Select>
+                </div>
+                <Icon
+                  className="trigger"
+                  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                  onClick={this.toggleSider}
+                />
+            </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Content style={{
+              background: '#fff', 
+              padding: 24, 
+              margin: 0,
+              minHeight: 280,}}
             >
-              <Option value="open">Open</Option>
-              <Option value="filled">Filled</Option>
-              <Option value="cancelled">Cancelled</Option>
-            </Select>
-
-            <Select
-              mode="multiple"
-              style={{ width: '100%' }}
-              placeholder="Select day of the week..."
-              onChange={this.handleSelectDay}
-            >
-              <Option value="monday">Monday</Option>
-              <Option value="tuesday">Tuesday</Option>
-              <Option value="wednesday">Wednesday</Option>
-              <Option value="thursday">Thursday</Option>
-              <Option value="friday">Friday</Option>
-              <Option value="saturday">Saturday</Option>
-            </Select>
-
-            <TimePicker
-              style={{ width: '100%' }}
-              placeholder="Select start time..."
-              minuteStep={5}
-              format = 'hh:mm a'
-              use12Hours
-              onChange={this.handleSelectDay}
-            >
-            </TimePicker >
-            <TimePicker
-              style={{ width: '100%' }}
-              placeholder="Select end time..."
-              minuteStep={5}
-              format = 'hh:mm a'
-              use12Hours
-              onChange={this.handleSelectDay}
-            >
-            </TimePicker >
-
-            <Select
-              mode="multiple"
-              style={{ width: '100%' }}
-              placeholder="Select department/major (you can also search here!)..."
-              onChange={this.handleSelectDeparment}
-            >
-              <Option value="american">American Studies</Option>
-              <Option value="anthropology">Anthropology</Option>
-              <Option value="art-histoy">Art & Art History</Option>
-              <Option value="biology">Biology</Option>
-              <Option value="business">Business</Option>
-              <Option value="career">Career And Life Planning</Option>
-              <Option value="biochemistry">Biochemistry</Option>
-              <Option value="chemistry">Chemistry</Option>
-              <Option value="classical-studies">Classical Studies</Option>
-              <Option value="communication">Communication</Option>
-              <Option value="computer-science">Computer Science</Option>
-              <Option value="cricital-media-cultural">Critical Media & Cultural Studies</Option>
-              <Option value="economics">Economics</Option>
-              <Option value="education">Education</Option>
-              <Option value="english">English</Option>
-              <Option value="environmental">Environmental Studies</Option>
-              <Option value="film">Film Studies</Option>
-              <Option value="global-health">Global Health</Option>
-              <Option value="health-professions">Health Professions Advising</Option>
-              <Option value="health-phyiscal">Health and Physical Education </Option>
-              <Option value="history">History</Option>
-              <Option value="honors">Honors Degree Program</Option>
-              <Option value="it">Information Technology</Option>
-              <Option value="interdisciplinary">Interdisciplinary</Option>
-              <Option value="latin-amerian-caribbean">Latin American & Caribbean</Option>
-              <Option value="mathematics">Mathematics</Option>
-              <Option value="middle-eastern-north-african">Middle Eastern & North African Studies</Option>
-              <OptGroup label="Modern Languages & Literatures">
-                <Option value="arabic">Arabic</Option>
-                <Option value="chinese">Chinese</Option>
-                <Option value="french">French</Option>
-                <Option value="german">German</Option>
-                <Option value="japanese">Japnese</Option>
-                <Option value="spanish">Spanish</Option>
-              </OptGroup>
-              <Option value="music">Music</Option>
-              <Option value="neuroscience">Neuroscience</Option>
-              <Option value="off-campus">Off-Campus Program Courses</Option>
-              <Option value="philosophy">Philosophy</Option>
-              <Option value="religion">Religion</Option>
-              <Option value="physics">Physics</Option>
-              <Option value="political-science">Political Science</Option>
-              <Option value="psychology">Psychology</Option>
-              <Option value="rcc">Rollins Conference Courses</Option>
-              <Option value="swag">Sexuality Women's & Gender Studies</Option>
-              <Option value="social-entrepreneurship">Social Entrepreneurship</Option>
-              <Option value="social-innovation">Social Innovation</Option>
-              <Option value="sociology">Sociology</Option>
-              <Option value="resource">Student Resource Center</Option>
-              <Option value="theatre">Theatre Arts</Option>
-              <Option value="dance">Dance</Option>
-              <OptGroup label="rFLA">
-                <Option value="rfla">rFLA</Option>
-                <Option value="ice">ICE</Option>
-                <Option value="imw">IMW</Option>
-                <Option value="mm">MM</Option>
-                <Option value="wcc">WCC</Option>
-              </OptGroup>
-            </Select>
-
-            <Select
-              mode="multiple"
-              style={{ width: '100%' }}
-              placeholder="Select course level (100, 200, etc.)..."
-              onChange={this.handleSelectLevel}
-            >
-              <Option value="100">100s</Option>
-              <Option value="200">200s</Option>
-              <Option value="300">300s</Option>
-              <Option value="400">400s</Option>
-            </Select>
-            
-          </div>
-      </Sider>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <Content style={{
-          background: '#fff', padding: 24, margin: 0, minHeight: 280,
-        }}
-        >
-          <div className='table'>
-            <Table dataSource={data} columns={columns} />
-          </div>
-        </Content>
+              <div className='table'>
+                <Table dataSource={data} columns={columns} />
+              </div>
+            </Content>
+          </Layout>
+        </Layout>
       </Layout>
-    </Layout>
-  </Layout>
-        <p className="App-intro">{this.state.data}</p>
-        <BackTop />
-      </div>
+      <p className="App-intro">{this.state.data}</p>
+      <BackTop />
+    </div>
     );
   }
 }
