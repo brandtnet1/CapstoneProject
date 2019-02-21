668146261ddefee7add02ab4bd0db59c7cc188f2
Rene Borr <Rborr@rollins.edu>
2019-02-21 15:18:17 -0500

    updated parser

--- server/parser.py
+++ server/parser.py
@@ -4,14 +4,21 @@ import json
 import re
 import pymongo
 from collections import OrderedDict
+import requests
 
 myclient = pymongo.MongoClient('mongodb+srv://admin:root@cluster0-pmazi.mongodb.net/Capstone?retryWrites=true')
 mydb = myclient['Capstone']
 mycol = mydb['courses']
 
-#Open HTML file
-f=codecs.open("html/rollins_course_schedule.html", 'r')
-sections = f.read()
+#Get HTML
+try:
+    sections = requests.get("https://bannerweb.rollins.edu/prod/owa/www_infotech.pkg_display_schedule.p_get_rschedule_spring?i_term_code=201901&i_campus_code=R")
+
+except:
+    print "Error, now loading from file"
+    f=codecs.open("html/rollins_course_schedule.html", 'r')
+    sections = f.read()
+
 sections = sections.split("</tr>")
 
 courses_json = []
@@ -26,21 +33,21 @@ sections = sections[first:]
 
 
 def parseCourse(section):
-   
+
     parts = section.split("<td");
-    
+
     #get status of course
     status = parts[1]
     if("Open" in status):
         status = True
-    else: 
+    else:
         status = False
-        
+
     #get seats available
     seats_available = parts[2]
     seats_available = re.sub('.*><.*?>', '', seats_available)
     seats_available = seats_available.strip()
-    
+
     # get course_number department & level & section
     cn_d_l = parts[3]
     cn_d_l = cn_d_l[25:len(cn_d_l) - 19]
@@ -51,7 +58,7 @@ def parseCourse(section):
     section = 1
     if(len(cn_d_l) == 4):
         section = cn_d_l[3]
-    
+
     #find name of course
     name = parts[4]
     name = name[18:len(name) - 12]
@@ -62,7 +69,7 @@ def parseCourse(section):
         credits = int(credits[23:len(credits) - 12])
     except:
         credits = 0
-    
+
 
     #get time
     times = parts[6]
@@ -74,8 +81,8 @@ def parseCourse(section):
     # times[len(time)]
     # ([0-9].:[0-9][0-9]-[0-9][0-9]:[0-9][0-9][a-zA-z])
     #([0-9]. ?:[0-9][0-9]-[0-9]. ?:[0-9][0-9][a-zA-z])
-    
-    
+
+
     #get day
     days = parts[7]
     days = re.sub('.*?><.*?>', '', days).replace('<br>', '')
@@ -93,7 +100,7 @@ def parseCourse(section):
     instructor = instructor[17:]
     instructor = instructor.split(" ")
     instructor = instructor[0] + " " + instructor[1]
-    
+
 
     #get competency
     competency = parts[10]
@@ -102,15 +109,15 @@ def parseCourse(section):
     #get note
     note = parts[11]
     note = note[21: len(note) - 12]
-    
-    course = [("Status", status), ("Seats_Available", seats_available), 
-             ("Course_Registration_Number", course_number), 
-             ("Course_Department", department), ("Course_Level", level), 
-             ("Course_Section", section), ("Course_Title", name), 
-             ("Course_Credits", credits), ("Times", times), 
+
+    course = [("Status", status), ("Seats_Available", seats_available),
+             ("Course_Registration_Number", course_number),
+             ("Course_Department", department), ("Course_Level", level),
+             ("Course_Section", section), ("Course_Title", name),
+             ("Course_Credits", credits), ("Times", times),
              ("Days", days), ("Location", location), ("Instructor", instructor),
              ("Competency", competency), ("Comments", note)]
-    
+
     # https://www.w3schools.com/python/python_mongodb_insert.asp
     mycol.insert_one(OrderedDict(course))
 
@@ -118,11 +125,11 @@ def parseCourse(section):
 i = 0;
 while(i<len(sections)):
     section = sections[i]
-    
+
     #if this sections contains course details, parse it
     if("Open" in section or "Filled" in section or "Cancelled" in section):
         courses_json.append(parseCourse(section))
-        
+
     i += 1
 
 print "Done"
